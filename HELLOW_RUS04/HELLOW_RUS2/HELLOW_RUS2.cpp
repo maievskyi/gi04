9673dc6009e4b691266b1101eab84f1c4707c324
maevskiy <maevsky.u@yandex.ua>
2014-12-22 21:10:29 +0200

    первое добавл

--- HELLOW_RUS04/HELLOW_RUS2/HELLOW_RUS2.cpp
+++ HELLOW_RUS04/HELLOW_RUS2/HELLOW_RUS2.cpp
@@ -11,6 +11,7 @@ int _tmain(int argc, _TCHAR* argv[])
 	setlocale(LC_ALL,"Rus"); /* Обязательно должно быть первым */
     char a1[100];
 	std::cout<<"Здравствуй весь мир!\n";
+	std::cout<<" 1  Здравствуй весь мир!\n";
 	cin>> a1;
 	cout << a1 <<endl;
 	system("pause");  //выв: "для продолжения ......."
