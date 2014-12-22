eefed6e4fcc2591486ec0b22d484a21fb8940cda
maevskiy <maevsky.u@yandex.ua>
2014-12-22 21:08:42 +0200

    первое занесение

---
+++ HELLOW_RUS04/HELLOW_RUS2/HELLOW_RUS2.cpp
@@ -0,0 +1,18 @@
+// HELLOW_RUS2.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <locale.h> /* Для русского языка */
+
+using namespace std;
+int _tmain(int argc, _TCHAR* argv[])
+{
+	setlocale(LC_ALL,"Rus"); /* Обязательно должно быть первым */
+    char a1[100];
+	std::cout<<"Здравствуй весь мир!\n";
+	cin>> a1;
+	cout << a1 <<endl;
+	system("pause");  //выв: "для продолжения ......."
+	return 0;
+}
