84ca185387bd9b29318863eb8ca820ed8cb6441e
maevskiy <maevsky.u@yandex.ua>
2014-12-22 21:15:58 +0200

    третье доб

--- HELLOW_RUS04/HELLOW_RUS2/HELLOW_RUS2.cpp
+++ HELLOW_RUS04/HELLOW_RUS2/HELLOW_RUS2.cpp
@@ -13,6 +13,7 @@ int _tmain(int argc, _TCHAR* argv[])
 	std::cout<<"Здравствуй весь мир!\n";
 	std::cout<<" 1  Здравствуй весь мир!\n";
 	std::cout<<"2  Здравствуй весь мир!\n";
+	std::cout<<"3  Здравствуй весь мир!\n";
 	cin>> a1;
 	cout << a1 <<endl;
 	system("pause");  //выв: "для продолжения ......."
